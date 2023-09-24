e4fbb09d16dc80e00410b53ddeea70e6f185498a
yasuda.chuma <86759515+yach36@users.noreply.github.com>
2023-09-24 20:45:52 +0900

    feat; redefine main func

--- main.go
+++ main.go
@@ -1,8 +1,17 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"strconv"
+	"time"
+
+	"github.com/spf13/viper"
+	adapterHttp "github.com/yach36/clean-arch-prac/adapter/http"
 )
 
 type MyHandler struct{}
@@ -11,6 +20,36 @@ func (h *MyHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "Hello World!")
 }
 
+func init() {
+	viper.SetConfigFile("config.json")
+	err := viper.ReadInConfig()
+	if err != nil {
+		panic(err)
+	}
+}
+
 func main() {
-	http.ListenAndServe(":8080", &MyHandler{})
+	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt)
+	defer stop()
+
+	r := adapterHttp.NewRouter()
+
+	serverAddr := viper.GetString("server.address")
+	server := &http.Server{
+		Addr:    serverAddr,
+		Handler: r,
+	}
+	timeout, _ := strconv.Atoi(viper.GetString("context.timeout"))
+	go func() {
+		<-ctx.Done()
+		ctx, cancel := context.WithTimeout(context.Background(), time.Duration(timeout)*time.Second)
+		defer cancel()
+		server.Shutdown(ctx)
+	}()
+
+	err := server.ListenAndServe()
+	if err != nil {
+		log.Fatalln(err)
+	}
+	log.Printf("server start running on %s\n", serverAddr)
 }
